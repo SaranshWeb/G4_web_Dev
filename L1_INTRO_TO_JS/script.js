0c7a1cf5e7bc640583c70c448cc7bc8afb151b32
Saransh Sharma <saranshsharmaji@gmail.com>
2025-07-02 10:25:28 +0530

    first commit

---
+++ L1_INTRO_TO_JS/script.js
@@ -0,0 +1,71 @@
+// console.log("Hello World");
+// console.log(a);
+// var a=123;
+// console.log(a);
+
+// function abc(){
+//     console.log("This is function abc");
+
+//     function hgf(){
+//         console.log("This is HGF");
+//     }
+//     hgf();
+// }
+
+// abc();
+
+// var b =300;
+
+// console.log(b);
+
+
+
+
+//////////////////////////////////////////////////////////////////////////
+
+
+
+
+// function abc(){
+//     var a = 100;
+//     var b = 200;
+//     var c = 300;
+
+//     function def(){
+//         var d = 400;
+//         function ijk(){
+//             console.log(a);
+//         }
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+// ////////////////////////////////////////////////////////////////////////////////
+
+
+// for(var i=0; i<=5; i++){
+//     // console.log(i);
+
+//     setTimeout(()=>{
+//         console.log(i);
+//     }, 2000)
+// }
+
+// for(let i=0; i<=5; i++){
+//     // console.log(i);
+
+//     setTimeout(()=>{
+//         console.log(i);
+//     }, 2000)
+// }
+
+
+
